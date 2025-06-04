686b53cd3b782100e71280c9726aa2ded2c29cf8
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2025-06-04 01:58:30 -0300

    feat: adiciona página de contato

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
