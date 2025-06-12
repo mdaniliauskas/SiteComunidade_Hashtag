f465112ea53409f49248621f3115d026f624126a
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2025-06-11 23:24:51 -0300

    Fix: ajusta ordem da checkbox "Lembrar Dados de Acesso" no login Refactor: revisa lógica de redirecionamento após login/criação de conta.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 app = Flask(__name__)
 
@@ -30,10 +30,18 @@ def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login realizado com sucesso para o e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
+        flash(f'Conta criada com sucesso para o e-mail: {form_criar_conta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
 
 if __name__ == "__main__":
